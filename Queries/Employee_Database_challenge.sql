b91ba35ed51ccb6cb19728dbc231dafb948f27c4
Deepali Bhardwaj <91810996+Sparkythephoenix@users.noreply.github.com>
2022-02-03 16:46:47 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,48 +1,52 @@
---Retrive employee details (emp_no,first-name,last-name etc) in to new retirement_title table by joining employees and titles table. 
+--Creating Rerirement Titles table
 SELECT e.emp_no,
-	   e.first_name,
-	   e.last_name,
-	   ti.title,
-	   ti.from_date,
-	   ti.to_date
-INTO retirement_title
-FROM employees AS e
-INNER JOIN titles AS ti
-ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no;
+	e.first_name,
+	e.last_name,
+	ttl.title,
+	ttl.from_date,
+	ttl.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ttl
+	ON (e.emp_no = ttl.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+SELECT * FROM retirement_titles;
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
-					first_name,
-					last_name,
-					title
+	first_name,
+	last_name,
+	title
 INTO unique_titles
-FROM retirement_title
+FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
-select*from unique_titles;
---retrive title wise employees count who are ready to retirement 
-SELECT COUNT(title) AS "Count",title
+
+SELECT * FROM unique_titles;
+
+-- Creating Retiring Titles table
+SELECT COUNT (title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
-ORDER BY "Count" DESC;
-select*from retiring_titles;
+ORDER BY count DESC;
 
---Retrive current  employees details who born in 1965 in to  mentorship_eligibility table.
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
-	   e.first_name,
-	   e.last_name,
-	   e.birth_date,
-	   de.from_date,
-	   de.to_date,
-	   ti.title
-INTO mentorship_eligibility
-FROM employees AS e
-INNER JOIN dept_emp AS de
-ON (e.emp_no=de.emp_no)
-INNER JOIN titles AS ti
-ON (e.emp_no=ti.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no; 
-select*from mentorship_eligibility;
+
+-- Creating Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ttl.title
+-- INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ttl
+	ON (e.emp_no = ttl.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
